c62f684b6ecfe2b6684c5e97f685b98d6472b367
Tharun <nunavaththarun103@gmail.com>
2026-02-03 21:24:25 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -72,5 +72,8 @@ def chatbot():
 
     return jsonify({"response": bot_reply})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
